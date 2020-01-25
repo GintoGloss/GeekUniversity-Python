834e8c9f09d325eb8b14611c58c40850853cc24e
GintoGloss <24726101+GintoGloss@users.noreply.github.com>
2020-01-25 15:51:56 +0700

    First lesson homework tasks

---
+++ task1.py
@@ -0,0 +1,13 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и
+# строк и сохраните в переменные, выведите на экран.
+
+value = 5
+old_value = value
+new_value = "5+5"
+value = new_value
+print(old_value, value)
+my_str1 = input("Введите строку: ")
+my_str2 = input("Введите строку: ")
+my_int1 = int(input("Введите целое число: "))
+my_int2 = int(input("Введите целое число: "))
+print(my_str1, my_int1, my_str2, my_int2)
